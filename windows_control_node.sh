7c3f45b00d32a84685af4c2129efa01538fedcb0
cxhacks_17 <chrisminepro@gmail.com>
2025-04-16 15:39:32 -0600

    Windows Ansible and Configure

--- windows_control_node.sh
+++ windows_control_node.sh
@@ -18,14 +18,13 @@ sudo snap install aws-cli --classic
 aws configure set region us-east-1
 aws configure set output json
 
-# Obtener el ID de la instancia
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-
 # Clonar el repositorio de Git
 cd /home/ubuntu
 git clone https://github.com/DxChrIs/Gestion-y-Automatizacion-de-Servidores.git
 cd Gestion-y-Automatizacion-de-Servidores
 
+# Obtener el ID de la instancia
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 # Detectar la IP local de la instancia
 MY_IP=$(hostname -I | awk '{print $1}')
 
@@ -39,13 +38,41 @@ aws ec2 describe-instances --filters "Name=tag:Role,Values=ad" --query "Reservat
 #File
 aws ec2 describe-instances --filters "Name=tag:Role,Values=file" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text > file_ips.txt
 
-# Combinar las IPs de web y sql en un solo archivo de inventario
-echo "[iis]" > inventory_iis.ini
-grep -v "$MY_IP" iis_ips.txt | while read ip; do
-    echo "$ip ansible_user=ubuntu ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_private_key_file=/home/ubuntu/ssh-code.pem" >> inventory_iis.ini
-done
+# === Crear archivo de inventario Windows para Ansible + WinRM ===
+echo "[windows]" > inventory_windows.ini
+cat iis_ips.txt ad_ips.txt file_ips.txt | grep -v "$MY_IP" >> inventory_windows.ini
 
-echo "[sql]" > inventory_sql.ini
-grep -v "$MY_IP" sql_ips.txt | while read ip; do
-    echo "$ip ansible_user=ubuntu ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_private_key_file=/home/ubuntu/ssh-code.pem" >> inventory_sql.ini
-done
+cat <<EOL >> inventory_windows.ini
+
+[windows:vars]
+ansible_user=Administrator
+ansible_password=Chris1853
+ansible_port=5985
+ansible_connection=winrm
+ansible_winrm_transport=basic
+ansible_winrm_server_cert_validation=ignore
+EOL
+
+# === Obtener el rol actual desde la instancia ===
+ROLE=$(aws ec2 describe-tags \
+  --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=Role" \
+  --query "Tags[0].Value" --output text)
+
+# === Ejecutar playbook según el rol ===
+case "$ROLE" in
+  "iis")
+    echo "Iniciando configuración para servidor IIS..."
+    ansible-playbook -i inventory_windows.ini windows-iis.yml
+    ;;
+  "ad")
+    echo "Iniciando configuración para Active Directory..."
+    ansible-playbook -i inventory_windows.ini windows-ad.yml
+    ;;
+  "file")
+    echo "Iniciando configuración para File Server..."
+    ansible-playbook -i inventory_windows.ini windows-file-server.yml
+    ;;
+  *)
+    echo "Rol no reconocido o no definido. No se ejecutará ningún playbook."
+    ;;
+esac
