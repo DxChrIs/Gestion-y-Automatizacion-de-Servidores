aa9303bf9062289a6c9b6f9b901e19db52845fa5
cxhacks_17 <chrisminepro@gmail.com>
2025-04-18 17:45:25 -0600

    IT WORKS YEAAAAAH. ANSIBLE FOR WINDOWS READY

--- iis_server_windows.ps1
+++ iis_server_windows.ps1
@@ -11,6 +11,8 @@
     Set-Item -Path WSMan:\localhost\Service\Auth\CredSSP -Value $true
     #Create a Firewall rule to allow WinRM HTTP inbound traffic
     New-NetFirewallRule -DisplayName "Allow WinRM HTTP" -Direction Inbound -LocalPort 5985 -Protocol TCP -Action Allow
+    New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Protocol ICMPv4 -IcmpType 8 -Direction Inbound -Action Allow
+    New-NetFirewallRule -DisplayName "Allow ICMPv6-In" -Protocol ICMPv6 -IcmpType 128 -Direction Inbound -Action Allow
     #Set LocalAccountTokenFilterPolicy
     New-ItemProperty -Name LocalAccountTokenFilterPolicy -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -PropertyType DWord -Value 1 -Force
     #Set Execution Policy to Unrestricted
