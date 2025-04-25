1f884704d010b7cd21106d99034a414474c92308
cxhacks_17 <chrisminepro@gmail.com>
2025-04-24 20:58:00 -0600

    Remove Private Subnets

--- main.tf
+++ main.tf
@@ -45,28 +45,6 @@ resource "aws_subnet" "public_subnet2" {
 }
 
 #############################################
-#             Private Subnet                #
-#############################################
-# Crear Subredes Privadas
-resource "aws_subnet" "private_subnet1" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.32.0/24"
-    availability_zone = "${var.region}a"
-    tags = {
-        Name = "subnet-private-1-${var.region}"
-    }
-}
-
-resource "aws_subnet" "private_subnet2" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.64.0/24"
-    availability_zone = "${var.region}b"
-    tags = {
-        Name = "subnet-private-2-${var.region}"
-    }
-}
-
-#############################################
 #            Internet Gateway               #
 #############################################
 # Crear el Internet Gateway
@@ -103,72 +81,6 @@ resource "aws_route_table_association" "public_subnet1_association" {
 resource "aws_route_table_association" "public_subnet2_association" {
     subnet_id      = aws_subnet.public_subnet2.id
     route_table_id = aws_route_table.public_route_table.id
-}
-
-#############################################
-#               Elastic IP                  #
-#############################################
-# Crear la Elastic IP
-resource "aws_eip" "nat_eip" {
-    domain = "vpc"
-    tags = {
-        Name = "eip-nat-${var.region}a"
-    }
-}
-
-#############################################
-#               NAT Gateway                 #
-#############################################
-# Crear el NAT Gateway
-resource "aws_nat_gateway" "nat_gateway" {
-    allocation_id = aws_eip.nat_eip.id
-    subnet_id     = aws_subnet.public_subnet1.id
-    tags = {
-        Name = "nat-private-${var.region}"
-    }
-}
-
-#############################################
-#            Private Route Table            #
-#############################################
-# Crear la tabla de rutas privadas para la subred privada 1
-resource "aws_route_table" "private_route_table_1" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "rtb-private-1-${var.region}a"
-    }
-}
-
-resource "aws_route" "private_route_1" {
-    route_table_id         = aws_route_table.private_route_table_1.id
-    destination_cidr_block = "0.0.0.0/0"
-    nat_gateway_id         = aws_nat_gateway.nat_gateway.id
-}
-
-# Asociar subred privada 1 con la tabla de rutas privadas 1
-resource "aws_route_table_association" "private_subnet1_association" {
-    subnet_id      = aws_subnet.private_subnet1.id
-    route_table_id = aws_route_table.private_route_table_1.id
-}
-
-# Crear la tabla de rutas privadas para la subred privada 2
-resource "aws_route_table" "private_route_table_2" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "rtb-private-2-${var.region}b"
-    }
-}
-
-resource "aws_route" "private_route_2" {
-    route_table_id         = aws_route_table.private_route_table_2.id
-    destination_cidr_block = "0.0.0.0/0"
-    nat_gateway_id         = aws_nat_gateway.nat_gateway.id
-}
-
-# Asociar subred privada 2 con la tabla de rutas privadas 2
-resource "aws_route_table_association" "private_subnet2_association" {
-    subnet_id      = aws_subnet.private_subnet2.id
-    route_table_id = aws_route_table.private_route_table_2.id
 }
 
 #############################################
